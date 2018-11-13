b9f0a42848de80af004f599a97b798a26c2bb167
Matt <Geczy@users.noreply.github.com>
2018-11-13 17:11:00 -0500

    Update open-vpn.sh

--- cron-scripts/open-vpn.sh
+++ cron-scripts/open-vpn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+if (( $(cat /proc/uptime | cut -d. -f1) < 60 )); then sleep 15; fi
+
 /sbin/iptables -D INPUT -i tun8 -p tcp --dport 22 -j ACCEPT
 /sbin/iptables -I INPUT -i tun8 -p tcp --dport 22 -j ACCEPT
 /usr/sbin/openvpn --config /var/spool/tesla.ovpn --daemon
-
