6e3844d867a65cfa896995cba22d17a245a879d5
Matt <Geczy@users.noreply.github.com>
2018-11-13 17:11:24 -0500

    Update startstopvpn.sh

--- cron-scripts/startstopvpn.sh
+++ cron-scripts/startstopvpn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+if (( $(cat /proc/uptime | cut -d. -f1) < 60 )); then sleep 15; fi
 
 /sbin/start-stop-daemon --start --quiet --make-pidfile --oknodo --background --pidfile /var/run/rob-vpn.pid --exec /usr/sbin/openvpn -- --config /var/spool/tesla.ovpn
