6e2e2a82f277b619a359afa826f686fc353ef2d5
Matt <Geczy@users.noreply.github.com>
2018-11-26 08:10:09 -0500

    Create reverse-tunnel.sh

---
+++ cron-scripts/reverse-tunnel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## To use: `ssh -p 33333 <name>@localhost` from server
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+while : ; do
+ssh -N -T -R 33333:localhost:22 -i ./.ssh/<YOUR KEY> -o ServerAliveInterval=3 -o StrictHostKeyChecking=no <USER>@<YOUR SERVER>
+sleep 600
+done
+
