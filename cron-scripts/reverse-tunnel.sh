5a5b0f25eac41db0061b12a1725b6e9fec51434a
Robby Grossman <robby@freerobby.com>
2019-01-01 18:21:01 -0500

    Assert port forwarding success on connection. If a port binding already exists on the remote host, we want to fail and keep retrying until that binding is gone.

--- cron-scripts/reverse-tunnel.sh
+++ cron-scripts/reverse-tunnel.sh
@@ -5,7 +5,7 @@
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
 while : ; do
-ssh -N -T -R 33333:localhost:22 -i ./.ssh/<YOUR KEY> -o ServerAliveInterval=3 -o StrictHostKeyChecking=no <USER>@<YOUR SERVER>
+ssh -N -T -R 33333:localhost:22 -i ./.ssh/<YOUR KEY> -o ServerAliveInterval=3 -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes <USER>@<YOUR SERVER>
 sleep 60
 done
 
