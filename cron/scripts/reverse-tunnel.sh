d640bfbe1ace7fd943e42826975a96c79495bc53
pk-r7 <35041244+pk-r7@users.noreply.github.com>
2019-05-03 23:48:52 -0700

    Update Reverse tunnel
    Current version creates a large number of connections, this version only establishes the tunnel when one does not exist.

--- cron/scripts/reverse-tunnel.sh
+++ cron/scripts/reverse-tunnel.sh
@@ -4,8 +4,10 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-while : ; do
-ssh -N -T -R 33333:localhost:22 -i ./.ssh/<YOUR KEY> -o ServerAliveInterval=3 -o StrictHostKeyChecking=no <USER>@<YOUR SERVER>
-sleep 60
+while true; do
+  RET=`ps ax | grep "ssh -N -T -R 33333:localhost:22" | grep -v "grep"`
+  if [ "$RET" = "" ];then
+    ssh -N -T -R 33333:localhost:22 -i {ssh key} -o ServerAliveInterval=3 -o StrictHostKeyChecking=no {username@server}
+  fi
+  sleep 300
 done
-
