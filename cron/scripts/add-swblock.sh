0133a44601b07bb727ae013b92b8389956364612
pk-r7 <35041244+pk-r7@users.noreply.github.com>
2019-05-05 13:11:31 -0700

    Add block for software updates

---
+++ cron/scripts/add-swblock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+if grep --quiet firmware.vn.teslamotors.com /etc/hosts; then
+  echo "Block in Place"
+else
+  echo "Adding block"
+  echo "0.0.0.0 firmware,vn.teslamotors.com"  >> /etc/hosts
+fi
