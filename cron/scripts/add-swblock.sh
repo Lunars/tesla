725f0e1e884be6d772766b49d81e21c9ec1cbc51
pk-r7 <35041244+pk-r7@users.noreply.github.com>
2019-05-07 15:50:53 -0700

    Update add-swblock.sh

--- cron/scripts/add-swblock.sh
+++ cron/scripts/add-swblock.sh
@@ -6,5 +6,5 @@ if grep --quiet firmware.vn.teslamotors.com /etc/hosts; then
   echo "Block in Place"
 else
   echo "Adding block"
-  echo "0.0.0.0 firmware,vn.teslamotors.com"  >> /etc/hosts
+  echo "0.0.0.0 firmware.vn.teslamotors.com"  >> /etc/hosts
 fi
