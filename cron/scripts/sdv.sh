d7f300d9dcc1bb20bcc79a8525bececf0b303783
pk-r7 <35041244+pk-r7@users.noreply.github.com>
2019-05-11 09:44:51 -0700

    Create sdv.sh

---
+++ cron/scripts/sdv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+/usr/local/bin/sdv {value goes here} [example GUI_eggWotMode 1 or GUI_eggWotMode 2]
+/usr/local/bin/sdv {other value goes here}
