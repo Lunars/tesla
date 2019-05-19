54811b99ba05f4fd25ba52fe395195af2a536882
pk-r7 <35041244+pk-r7@users.noreply.github.com>
2019-05-19 10:09:25 -0700

    Update sdv.sh

--- cron/scripts/sdv.sh
+++ cron/scripts/sdv.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-/usr/local/bin/sdv {value goes here} [example GUI_eggWotMode 1 or GUI_eggWotMode 2]
-/usr/local/bin/sdv {other value goes here}
+STATE=$(/usr/local/bin/lv GUI_eggWotMode | cut -d "\"" -f2)
+WANTSTATE="Preparing"
+
+echo "state is" $STATE
+echo "state wanted is" $WANTSTATE
+
+if ! [ "$STATE" = "$WANTSTATE" ] ; then
+   /usr/local/bin/sdv GUI_eggWotMode 1
+   echo "Enabling GUI_eggWotMode 1"
+else
+   echo "Already Enabled"
+fi
