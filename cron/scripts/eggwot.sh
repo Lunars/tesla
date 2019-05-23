fee7c31d1c92a5af9ed1764c22ac5fdb47e19b9b
Matt <Geczy@users.noreply.github.com>
2019-05-23 17:58:08 -0400

    Update eggwot.sh

--- cron/scripts/eggwot.sh
+++ cron/scripts/eggwot.sh
@@ -3,13 +3,14 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
 STATE=$(/usr/local/bin/lv GUI_eggWotMode | cut -d "\"" -f2)
 WANTSTATE="Preparing"
+MODE=1
 
 echo "state is" $STATE
 echo "state wanted is" $WANTSTATE
 
 if ! [ "$STATE" = "$WANTSTATE" ] ; then
-   /usr/local/bin/sdv GUI_eggWotMode 1
-   echo "Enabling GUI_eggWotMode 1"
+   /usr/local/bin/sdv GUI_eggWotMode $MODE
+   echo "Enabling GUI_eggWotMode $MODE"
 else
    echo "Already Enabled"
 fi
