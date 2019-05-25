918a11243117b4f1648a0c1c7ff00b1d5c275c96
Matt <Geczy@users.noreply.github.com>
2019-05-24 20:06:56 -0400

    Delete add-ssh-key-tesla.sh

--- cron/scripts/add-ssh-key-tesla.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /home/tesla/.ssh 2>&1
-if grep --quiet PARTIAL_KEY_GOES_HERE /home/tesla/.ssh/authorized_keys; then
-  echo "Key already present for tesla user"
-else
-  echo "Key not present for tesla user, adding it..."
-  echo "ssh-rsa KEY_GOES_HERE"  >> /home/tesla/.ssh/authorized_keys
-fi
