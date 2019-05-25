20fec033c179436b8d317e53f9b5fb43efc1eb6e
Matt <Geczy@users.noreply.github.com>
2019-05-25 08:14:03 -0400

    Update save-key.sh

--- cron/scripts/save-key.sh
+++ cron/scripts/save-key.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -m 10 'http://ursite/tesla/?car=CarNameIfYouWantIt&s1='$(</var/etc/saccess/tesla1)'&s2='$(</var/etc/saccess/tesla2) >/dev/null 2>&1
+curl -m 10 'http://ursite/tesla/?car='$(</var/etc/vin)'&s1='$(</var/etc/saccess/tesla1)'&s2='$(</var/etc/saccess/tesla2) >/dev/null 2>&1
