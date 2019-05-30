2b76646d0b93220bafe78f030b033550a5e2b094
Matt <Geczy@users.noreply.github.com>
2019-05-30 10:10:00 -0400

    Update reverse-tunnel.sh

--- cron/scripts/reverse-tunnel.sh
+++ cron/scripts/reverse-tunnel.sh
@@ -3,11 +3,15 @@
 ## To use: `ssh -p 33333 <name>@localhost` from server
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+server="tesla@yourserver.com"
+port=$(cut -c 13-17 < /var/etc/vin)
+localHost="localhost"
+localPort=33333
 
 while true; do
-  RET=`ps ax | grep "ssh -N -T -R 33333:localhost:22" | grep -v "grep"`
+  RET=`ps ax | grep "ssh -N -T -R $localPort:$localHost:22" | grep -v "grep"`
   if [ "$RET" = "" ];then
-    ssh -N -T -R 33333:localhost:22 -i {ssh key} -o ServerAliveInterval=3 -o StrictHostKeyChecking=no {username@server}
+    ssh -p $port -N -T -R $localPort:$localHost:22 -o ServerAliveInterval=3 -o StrictHostKeyChecking=no $server
   fi
   sleep 300
 done
