0c35243967a4928d55c8a05bd550ed17a4dd65dc
Matt <Geczy@users.noreply.github.com>
2019-06-02 10:21:58 -0400

    Update eggwot.sh

--- cron/scripts/eggwot.sh
+++ cron/scripts/eggwot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+# Mode 1 shows the battery temperature level on the IC display, as a purple indicator on the same menu as the energy graph
+# Mode 2 does what 1 does, in addition to showing drive unit statistics on the left IC menu
+
 STATE=$(/usr/local/bin/lv GUI_eggWotMode | cut -d "\"" -f2)
 WANTSTATE="Preparing"
 MODE=1
