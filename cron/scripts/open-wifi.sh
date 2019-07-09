f257c3c4f29bb14f07473fd228569a422e5a224a
Matt <Geczy@users.noreply.github.com>
2019-07-09 12:21:50 -0400

    Update open-wifi.sh

--- cron/scripts/open-wifi.sh
+++ cron/scripts/open-wifi.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# When on a wifi network, this script will allow you to ssh to the local ip 
-# You have to be on the same wifi network as the car 
-# Uses port 229 for ssh
-# Useful if you're on the road and you open a hotspot on your phone, then connect car to phone's wifi hotspot. Now you can ssh to the car 
+# When on a wifi network, this script will allow you to ssh to the local ip
+# You have to be on the same wifi network as the car
+# Uses port $WIFIPORT for ssh
+# Useful if you're on the road and you open a hotspot on your phone, then connect car to phone's wifi hotspot. Now you can ssh to the car
 
-# ssh -p 229 tesla1@192.168.1.154 
+# ssh -p $WIFIPORT tesla1@192.168.1.154
 
-if (( $(cat /proc/uptime | cut -d. -f1) < 60 )); then sleep 15; fi
+if (($(cat /proc/uptime | cut -d. -f1) < 60)); then sleep 15; fi
+
+WIFIPORT=229
+PARROTIP=192.168.20.2
 
 /sbin/iptables -D INPUT -i parrot -p tcp -m tcp --dport 22 -j ACCEPT
 /sbin/iptables -I INPUT -i parrot -p tcp -m tcp --dport 22 -j ACCEPT
-{ echo; sleep 1; echo; sleep 1; echo "iptables -t nat -D PREROUTING -i mlan0 -p tcp -m tcp --dport 229 -j DNAT --to-destination 192.168.20.2:22"; sleep 1; echo "iptables -t nat -I PREROUTING -i mlan0 -p tcp -m tcp --dport 229 -j DNAT --to-destination 192.168.20.2:22"; sleep 1; echo "iptables -D INPUT -p tcp --dport 229 -j ACCEPT"; sleep 1; echo "iptables -I INPUT -p tcp --dport 229 -j ACCEPT"; sleep 1; } | socat - tcp:parrot:telnet
+
+{
+    echo
+    sleep 1
+    echo
+    sleep 1
+    echo "iptables -t nat -D PREROUTING -i mlan0 -p tcp -m tcp --dport $WIFIPORT -j DNAT --to-destination $PARROTIP:22"
+    sleep 1
+    echo "iptables -t nat -I PREROUTING -i mlan0 -p tcp -m tcp --dport $WIFIPORT -j DNAT --to-destination $PARROTIP:22"
+    sleep 1
+    echo "iptables -D INPUT -p tcp --dport $WIFIPORT -j ACCEPT"
+    sleep 1
+    echo "iptables -I INPUT -p tcp --dport $WIFIPORT -j ACCEPT"
+    sleep 1
+} | socat - tcp:parrot:telnet
