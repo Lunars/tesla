7b6360e5e31ca0bc270601374465bed4e180e430
Matt <Geczy@users.noreply.github.com>
2019-07-13 08:26:08 -0400

    Update wakedisplay.sh

--- cron/scripts/wakedisplay.sh
+++ cron/scripts/wakedisplay.sh
@@ -1,3 +1,3 @@
 #this script will wake your bench cid display if you add it to cron.
-#@reboot bash /var/spool/wakedisplay.sh
+#@reboot bash /var/root/lunars/cron/scripts/wakedisplay.sh
 curl -L "http://192.168.90.100:4070/_data_set_value_request_?name=CD_displayState&value=2"
