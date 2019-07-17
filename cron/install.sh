115b52bb3fea2a95ffa8637cc08ab5d1e9afe5e4
Matt Gates <info@mgates.me>
2019-07-17 12:12:08 -0400

    Update the installer

--- cron/install.sh
+++ cron/install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Credit to FreedomEV for the script
-
+# Credit to FreedomEV for the install script
 echo Script to install Lunars
 
-#detect if we are running chrooted by checking if the root of the init process
-#is the same as the root of this process
+# Detect if we are running chrooted by checking if the root of the init process is the same as the root of this process
 if [[ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]] 
 then
  echo This is not supposed to run chrooted
@@ -23,11 +21,20 @@ fi
 
 echo Not yet installed: OK
 
-#installing crontab
+# Downloading repo to CID
+mkdir -p /var/root/lunars
+curl -SL https://github.com/Lunars/tesla/tarball/master -o lunars.zip
+tar xvf ./lunars.zip -C /var/root/lunars/
+rm ./lunars.zip
+mv /var/root/lunars/Lunars-tesla*/* /var/root/lunars/
+rm -rf /var/root/lunars/Lunars-tesla*
+
+# Installing crontab
 crontab -l > /tmp/crontab
 echo '@reboot /bin/bash /var/root/lunars/cron/scripts/on-reboot.sh > /dev/null 2>&1 &' >> /tmp/crontab
 cat /tmp/crontab | crontab || exit 6
 echo Crontab installed: OK
 
+# Starting scripts
 /bin/bash /var/root/lunars/cron/scripts/on-reboot.sh > /dev/null 2>&1 &
 echo Lunars installed! Have fun!
