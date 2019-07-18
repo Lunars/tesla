9fe99d0e830b55147e06de66557b2096d139c026
Matt <Geczy@users.noreply.github.com>
2019-07-18 13:10:53 -0400

    Update mount-modfiles.sh
    chmod +x if its a bin script

--- src/scripts/once/mount-modfiles.sh
+++ src/scripts/once/mount-modfiles.sh
@@ -10,28 +10,26 @@ saveLocation="/var/root/lunars/src/overwrite-files"
 ENABLE=true
 
 if [ "$ENABLE" == "false" ]; then
-    echo "Script not enabled, quitting"
-    exit 1
+  echo "Script not enabled, quitting"
+  exit 1
 fi
 
 mkdir -p "$saveLocation/usr/local/bin/"
-for bindmount in $(mount | grep bind | awk '{ print $1 }')
-do
- /bin/umount $bindmount
+for bindmount in $(mount | grep bind | awk '{ print $1 }'); do
+  chmod +x $bindmount
+  /bin/umount $bindmount
 done
 
 cd $saveLocation$1
-for modfile in $(find . -type f)
-do
- #in case the containing dir isn't there yet
- mkdir -p /$(dirname $modfile) 2> /dev/null
- #displays warnings because mounting a file
- /bin/mount --bind $modfile /$modfile 2> /dev/null
- /bin/mount -o remount,ro /$modfile 2> /dev/null
+for modfile in $(find . -type f); do
+  #in case the containing dir isn't there yet
+  mkdir -p /$(dirname $modfile) 2>/dev/null
+  #displays warnings because mounting a file
+  /bin/mount --bind $modfile /$modfile 2>/dev/null
+  /bin/mount -o remount,ro /$modfile 2>/dev/null
 done
 
 #if theres a changed init script, reload upstart
-if [ -d $saveLocation$1/etc/init/ ]
-then
- /usr/cid-slash-sbin/initctl reload-configuration
+if [ -d $saveLocation$1/etc/init/ ]; then
+  /usr/cid-slash-sbin/initctl reload-configuration
 fi
