bf2780fcef1951ff4f029507a684a8d6c0793e97
Matt <Geczy@users.noreply.github.com>
2019-07-18 12:46:39 -0400

    Update CopyAcct.sh

--- src/scripts/every5/CopyAcct.sh
+++ src/scripts/every5/CopyAcct.sh
@@ -2,5 +2,12 @@
 
 # Copies the tokens from CID to IC since you can always SSH to IC 
 
+ENABLE=false
+
+if [ "$ENABLE" == "false" ]; then
+    echo "Script not enabled, quitting"
+    exit 1
+fi
+
 scp /var/etc/saccess/tesla1 192.168.90.101:/home/tesla/tesla1
 scp /var/etc/saccess/tesla2 192.168.90.101:/home/tesla/tesla2
