730a25416af0496992177dbfa52aa16a6a72db99
Matt <Geczy@users.noreply.github.com>
2019-07-18 12:45:06 -0400

    Update open-vpn.sh

--- src/scripts/every5/open-vpn.sh
+++ src/scripts/every5/open-vpn.sh
@@ -4,6 +4,11 @@
 file="/var/root/lunars/src/tesla.ovpn"
 vpn="tun8"
 
+if grep -q "YOURIPADDRESSHERE" $file; then
+    echo "Script not yet setup, quitting"
+    exit 1
+fi
+
 if (( $(cat /proc/uptime | cut -d. -f1) < 60 )); then sleep 15; fi
 
 /sbin/iptables -D INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
