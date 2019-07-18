6d130b8922a351e8804bea1a4e184fea9a9f72d6
Matt <Geczy@users.noreply.github.com>
2019-07-18 12:46:52 -0400

    Update eggwot.sh

--- src/scripts/every5/eggwot.sh
+++ src/scripts/every5/eggwot.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+ENABLE=false
+
+if [ "$ENABLE" == "false" ]; then
+    echo "Script not enabled, quitting"
+    exit 1
+fi
+
 # Mode 1 shows the battery temperature level on the IC display, as a purple indicator on the same menu as the energy graph
 # Mode 2 does what 1 does, in addition to showing drive unit statistics on the left IC menu
 # Can also be used to control other sdv commands such as GUI_developerMode (values= true and false for $MODE)
