d18dad094cf5d127911be40d2a0f1473575f2ad3
Matt <Geczy@users.noreply.github.com>
2019-07-18 12:47:57 -0400

    Update block-tesla-ssh.sh

--- src/scripts/once/block-tesla-ssh.sh
+++ src/scripts/once/block-tesla-ssh.sh
@@ -1,5 +1,13 @@
 #! bin/bash
 # Block Tesla from SSHing to car
+
+ENABLE=false
+
+if [ "$ENABLE" == "false" ]; then
+    echo "Script not enabled, quitting"
+    exit 1
+fi
+
 /sbin/iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
 /sbin/iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j LOG
 /sbin/iptables -I INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
