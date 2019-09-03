a7126ece66157ce80a191b80435435171b264281
Matt <Geczy@users.noreply.github.com>
2019-09-03 11:06:21 -0400

    remove absolute paths

--- src/scripts/every5/open-vpn.sh
+++ src/scripts/every5/open-vpn.sh
@@ -11,6 +11,6 @@ fi
 
 if (( $(cat /proc/uptime | cut -d. -f1) < 60 )); then sleep 15; fi
 
-/sbin/iptables -D INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
-/sbin/iptables -I INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
-/usr/sbin/openvpn --config $file --daemon
+iptables -D INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
+openvpn --config $file --daemon
