828c47c7862659136010cbab7885e30e19dc0ff2
Matt <Geczy@users.noreply.github.com>
2019-09-03 11:05:49 -0400

    remove iptables absolute path

--- src/scripts/once/block-tesla-ssh.sh
+++ src/scripts/once/block-tesla-ssh.sh
@@ -8,7 +8,7 @@ if [ "$ENABLE" == "false" ]; then
     exit 1
 fi
 
-/sbin/iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
-/sbin/iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j LOG
-/sbin/iptables -I INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
-/sbin/iptables -I INPUT -i tun0 -p tcp -m tcp --dport 22 -j LOG
+iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
+iptables -D INPUT -i tun0 -p tcp -m tcp --dport 22 -j LOG
+iptables -I INPUT -i tun0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
+iptables -I INPUT -i tun0 -p tcp -m tcp --dport 22 -j LOG
