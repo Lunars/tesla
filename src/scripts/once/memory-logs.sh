1881563afe99ceebac354fd8a94e34bb11c9ceab
Matt <Geczy@users.noreply.github.com>
2019-09-11 16:51:47 -0400

    Update memory-logs.sh

--- src/scripts/once/memory-logs.sh
+++ src/scripts/once/memory-logs.sh
@@ -13,21 +13,7 @@ ISTMPFS=$(/bin/mount | /bin/grep -c "/var/log type tmpfs")
 
 if [ "$ISTMPFS" == 0 ]; then
   /bin/mount -t tmpfs -o size=100M tmpfs /var/log; /sbin/initctl restart rsyslog
-  kill $(cat /etc/sv/camera0-capture/log/supervise/pid)
-  kill $(cat /etc/sv/camera1-capture/log/supervise/pid)
-  kill $(cat /etc/sv/camera2-capture/log/supervise/pid)
-  kill $(cat /etc/sv/camera3-capture/log/supervise/pid)
-  kill $(cat /etc/sv/crashloghelper/log/supervise/pid)
-  kill $(cat /etc/sv/crashlognotify/log/supervise/pid)
-  kill $(cat /etc/sv/dashcam/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-client/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-eventlogs/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-grablogs/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-historylogs/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-proxy/log/supervise/pid)
-  kill $(cat /etc/sv/hermes-teleforce/log/supervise/pid)
-  kill $(cat /etc/sv/odin-engine/log/supervise/pid)
-  kill $(cat /etc/sv/usbupdate-server/log/supervise/pid)
+  kill $(cat /etc/sv/*/log/supervise/pid)
   echo SYSLOG now on TMPFS
 else
   echo SYSLOG already on TMPFS
