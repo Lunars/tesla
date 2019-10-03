39be58bd770912bc9d6615dafc80260ba4ba4655
Matt <Geczy@users.noreply.github.com>
2019-10-03 15:44:14 -0400

    Update startstopvpn.sh

--- src/scripts/every5/startstopvpn.sh
+++ src/scripts/every5/startstopvpn.sh
@@ -2,10 +2,13 @@
 if (($(cat /proc/uptime | cut -d. -f1) < 60)); then sleep 15; fi
 
 file="/var/root/lunars/src/tesla.ovpn"
+vpn="tun8"
 
 if grep -q "YOURIPADDRESSHERE" $file; then
     echo "Script not yet setup, quitting"
     exit 1
 fi
 
+iptables -D INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
 /sbin/start-stop-daemon --start --quiet --make-pidfile --oknodo --background --pidfile /var/run/rob-vpn.pid --exec /usr/sbin/openvpn -- --config $file
