fb1033cc84dddcf9e73edd44516a6b2910397e62
Wido den Hollander <wido@widodh.nl>
2019-10-03 14:04:19 +0200

    Update reverse SSH
    Use the last 4 digits of the VIN and SSH to the remote server on port 22
    
    Signed-off-by: Wido den Hollander <wido@widodh.nl>

--- src/scripts/forever/reverse-tunnel.sh
+++ src/scripts/forever/reverse-tunnel.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
-## To use: `ssh -p 33333 <name>@localhost` from server
+#
+# Set up a reverse SSH tunnel to a remote server
+#
+# Make sure the user tesla (default) can log in without a password
+#
+# On that server SSH back to your car using:
+#
+# ssh -p <last4ofvin> <name>@localhost
 
 server="tesla@yourserver.com"
-port=$(cut -c 13-17 < /var/etc/vin)
-localHost="33333:localhost"
+port=$(cut -c 14-17 < /var/etc/vin)
 
 if [ "$server" == "tesla@yourserver.com" ]; then
   echo "Script not yet setup, quitting"
@@ -12,9 +17,9 @@ if [ "$server" == "tesla@yourserver.com" ]; then
 fi
 
 while : ; do
-  RET=`ps ax | grep "$localHost:22" | grep -v "grep"`
+  RET=`ps ax | grep "${port}:localhost:22" | grep -v "grep"`
   if [ "$RET" = "" ];then
-    ssh -p $port -N -T -R $localHost:22 -o ServerAliveInterval=3 -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes $server
+    ssh -N -T -R ${port}:localhost:22 -o ServerAliveInterval=3 -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes $server
   fi
   sleep 60
 done
