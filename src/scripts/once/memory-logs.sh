8dfd5cbd0cdc65f0736f26c516b32301a148210e
Matt <Geczy@users.noreply.github.com>
2019-10-13 15:45:57 -0400

    Update memory-logs.sh

--- src/scripts/once/memory-logs.sh
+++ src/scripts/once/memory-logs.sh
@@ -21,8 +21,8 @@ if [ "$ISTMPFS" == 0 ]; then
     pkill -HUP valhalla_server
     restart rsyslog
     
-    ssh ic mount -t tmpfs -o size=100M tmpfs /var/log
-    ssh ic mkdir /var/log/ntpstats /var/log/mgetty
-    ssh ic service ntp restart 
-    ssh ic restart rsyslog     
+    ssh ic "mount -t tmpfs -o size=100M tmpfs /var/log"
+    ssh ic "mkdir /var/log/ntpstats /var/log/mgetty"
+    ssh ic "service ntp restart"
+    ssh ic "restart rsyslog"
 fi
