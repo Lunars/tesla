ff03fd92ee6a991e86d280b37a515ad39ccddd98
Matt <Geczy@users.noreply.github.com>
2019-10-16 09:08:42 -0400

    Update save-update.sh

--- src/scripts/save-update.sh
+++ src/scripts/save-update.sh
@@ -3,8 +3,11 @@
 # Run this script in the background, because it may take a while to complete
 # Usage: bash save-update.sh MODE &
 
-SERVER="tesla@yourHost.com"
-PORT=$(cut -c 13-17 </var/etc/vin)
+PORT=$(cut -c 14-17 </var/etc/vin)
+SERVER="tesla@yourserver.com"
+
+# Non standard sshd ports can be set like so
+#server="tesla@yourserver.com -p ${port}"
 
 die() {
     echo "ERROR: $1" >&2
@@ -52,7 +55,7 @@ elif [ "$MODE" = usb ]; then
     umount /disk/usb.*/
 elif [ "$MODE" = ssh ]; then
     echo "Saving to /tmp/$NEWVER.image on remote server"
-    dd if=/dev/mmcblk0p$OFFLINEPART bs=64 count=$NEWSIZE | ssh -p $PORT $SERVER "dd of=/tmp/$NEWVER.image"
+    dd if=/dev/mmcblk0p$OFFLINEPART bs=64 count=$NEWSIZE | ssh $SERVER "dd of=/tmp/$NEWVER.image"
 else
     die "MODE must be one of usb | cid | ssh"
 fi
