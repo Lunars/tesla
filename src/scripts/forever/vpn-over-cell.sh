52f88a987ae14a5628c501b9e0d5c466b0b90584
Matt <Geczy@users.noreply.github.com>
2019-11-02 17:26:19 -0400

    Update vpn-over-cell.sh

--- src/scripts/forever/vpn-over-cell.sh
+++ src/scripts/forever/vpn-over-cell.sh
@@ -19,19 +19,25 @@ if grep -q "YOURIPADDRESSHERE" $OVPN; then
     exit 1
 fi
 
-while inotifywait -e modify $DHCPLEASES; do
-    echo "Found cell change, updating route"
-
+function addVPNRoute {
     VPNIP=$(awk '/^remote /' $OVPN | awk '{print $2}' )
     CELLIP=$(ip r | grep wwan0 | grep -Pom 1 '[0-9.]{7,15}' | tail -1)
-    
+
     service openvpn stop
     route delete $VPNIP > /dev/null 2>&1
-    
+
     if [ ! -z "$CELLIP" ]; then
         ROUTERIP=$(grep $CELLIP -A 3 $DHCPLEASES | tail -1 | grep -Pom 1 '[0-9.]{7,15}')
         route add $VPNIP gw $ROUTERIP
     fi
-    
+
     service openvpn start
+}
+
+# Called once at least on start, so that the appropriate routes can be added
+addVPNRoute
+
+while inotifywait -e modify $DHCPLEASES; do
+    echo "Found cell change, updating route"
+    addVPNRoute
 done
