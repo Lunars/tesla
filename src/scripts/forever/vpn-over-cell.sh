3a68792e59218afdf35598d4222663567d52161a
Matt <Geczy@users.noreply.github.com>
2019-11-02 10:50:21 -0400

    Create vpn-over-cell.sh

---
+++ src/scripts/forever/vpn-over-cell.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ENABLE=false
+
+if [ "$ENABLE" == "false" ]; then
+    echo "Script not enabled, quitting"
+    exit 1
+fi
+
+DHCPLEASES="/var/lib/dhcp3/dhclient.wwan0.leases"
+GWIP="192.168.90.102"
+OVPN="/home/lunars/src/tesla.ovpn"
+
+while inotifywait -e modify $DHCPLEASES; do
+	echo "Found cell change, updating route"
+
+	VPNIP=$(awk '/^remote /' $OVPN | awk '{print $2}' )
+	CELLIP=$(ip r | grep wwan0 | grep -Pom 1 '[0-9.]{7,15}' | tail -1)
+	ROUTERIP=$(cat $DHCPLEASES | grep $CELLIP -A 3 | tail -1 | grep -Pom 1 '[0-9.]{7,15}')
+
+	service openvpn stop
+	route delete $GWIP
+	route add $VPNIP $GWIP $ROUTERIP
+	service openvpn start
+done
