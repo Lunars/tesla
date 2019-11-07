08221bd0e685bf5fe4d9d01132bd26614c5a9c40
Matt <Geczy@users.noreply.github.com>
2019-11-07 07:17:16 -0500

    Update wipe-update.sh

--- src/scripts/wipe-update.sh
+++ src/scripts/wipe-update.sh
@@ -4,3 +4,8 @@
 
 initctl stop cid-updater && rm -rf /var/spool/*-updater/* && initctl start cid-updater
 ssh root@ic "initctl stop ic-updater && rm -rf /var/spool/*-updater/* && initctl start ic-updater"
+
+sdv GUI_softwareUpdateSecondaryDownload Idle
+sdv GUI_softwareUpdatePrimaryDownload Idle
+sdv GUI_softwareUpdatePackageDownloadProgress 0
+sdv GUI_softwareUpdateStatus idle
