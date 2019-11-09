1ed2be734d888596897bbe9f387dfe26b3d47d56
Matt <Geczy@users.noreply.github.com>
2019-11-09 09:09:57 -0500

    Update save-update.sh

--- src/scripts/save-update.sh
+++ src/scripts/save-update.sh
@@ -7,29 +7,42 @@
 
 # Usage: bash save-update.sh HOST MODE &
 
-PORT=$(cut -c 14-17 </var/etc/vin)
-
-SSHSERVER="tesla@yourserver.com -p 22"
-FTPSERVER="username:password@ftp.example.com:21"
-
-die() {
+function die {
     echo "ERROR: $1" >&2
     exit 1
 }
 
-# If FTP username has an @, replace with %40
-USERNAME=$(echo $FTPSERVER | cut -d: -f1)
-ENCODEDUSERNAME=${USERNAME/@/%40}
-FTPSERVER=${FTPSERVER/$USERNAME/$ENCODEDUSERNAME}
-
 [[ $# < 2 ]] && die "Must have arguments of bash save-update.sh ic|cid usb|internal|ssh|ftp"
-HOST="$1"
-MODE="$2"
 
-# Quick fail safe to move from cid to ic
-if [ "$HOST" = ic ] && [ $(hostname) = cid ]; then
-    die "Run this script from ic"
-fi
+function initializeVariables {
+    # $PORT not used anywhere. Just left here as a helper in case you have non-standard ftp/ssh ports
+    PORT=$(cut -c 14-17 </var/etc/vin)
+    
+    SSHSERVER="tesla@yourserver.com -p 22"
+    FTPSERVER="username:password@ftp.example.com:21"
+}
+
+
+function cleanFTPUsername { 
+    # If FTP username has an @, replace with %40
+    USERNAME=$(echo $FTPSERVER | cut -d: -f1)
+    ENCODEDUSERNAME=${USERNAME/@/%40}
+    FTPSERVER=${FTPSERVER/$USERNAME/$ENCODEDUSERNAME}
+
+    HOST="$1"
+    MODE="$2"
+}
+
+function validateIC {
+    # Quick fail safe to move from cid to ic
+    if [ "$HOST" = ic ] && [ $(hostname) = cid ]; then
+        die "Run this script from ic"
+    fi
+}
+
+initializeVariables
+cleanFTPUsername
+validateIC
 
 PARTITIONPREFIX=$([ "$HOST" = ic ] && echo "mmcblk3p" || echo "mmcblk0p")
 STATUS=$([ "$HOST" = ic ] && curl http://ic:21576/status || curl http://cid:20564/status)
@@ -46,6 +59,32 @@ else
     die "Could not determine offline partition"
 fi
 
+function saveAPE {
+    # Download gateway config in case it doesn't exist
+    echo "Attempting to download gateway config..."
+    [ -x "$(command -v get-gateway-config.sh)" ] && [ "$HOST" = cid ] && bash get-gateway-config.sh
+
+    APELOCATION="/home/cid-updater/ape-cache.ssq"
+    if [ -f "$APELOCATION" ]; then
+        echo "Found APE image at $APELOCATION"
+        APE="ape0"
+        MODEL=$(< /var/etc/dashw)
+        
+        [ "$MODEL" == 2 ] && APE="ape2"
+        [ "$MODEL" == 3 ] && APE="ape25"
+        [[ "$MODEL" -ge 4 ]] && APE="ape3"
+
+        if [ "$APE" == "ape0" ]; then
+            echo "Found ape image but could not determine APE version from dashw: $MODEL"
+            echo "Defaulting to .ape0 so we still save this file"
+        fi
+
+        curl -T $APELOCATION ftp://$FTPSERVER/~/$NEWVER.$APE
+    else
+        echo "Skipping transfer of ape-cache.ssq, did not find file at $APELOCATION"
+    fi
+}
+
 if [ "$MODE" = internal ]; then
     echo "Saving to /tmp/$NEWVER.image"
     dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 of=/tmp/$NEWVER.image count=$NEWSIZE
@@ -59,24 +98,22 @@ elif [ "$MODE" = ssh ]; then
     echo "Saving to /tmp/$NEWVER.image on remote server via SSH"
     dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | ssh $SSHSERVER "dd of=/tmp/$NEWVER.image"
 elif [ "$MODE" = ftp ]; then
+    saveAPE
     echo "Saving to ~/$NEWVER.image on remote server via FTP"
     dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | curl -T - ftp://$FTPSERVER/~/$NEWVER.image
 else
     die "MODE must be one of usb | internal | ssh | ftp"
 fi
 
+function saveNavigon {
+    # @TODO: Add navigon image to arguments
 
-######### START NAVIGON
-# @TODO: Add navigon image to arguments
-
-#NAVPART=$(cat /proc/mounts | grep opt/nav | head -n1 | cut -d " " -f1)
-#NAVMOUNT=$(cat /proc/mounts | grep opt/nav | head -n1 | awk '{print $2;}')
-#NAVSIZE=$(echo "$STATUS" | grep 'Online map package size:' | awk -F'Online map package size: ' '{print $2}' | awk '{print $1/64}')
-#NAVVERSION=$(cat $NAVMOUNT/VERSION | head -n1 | cut -d " " -f1)
-#echo "Version $NAVVERSION is mounted at $NAVPART $NAVMOUNT ($NAVSIZE)"
-# dd if=$NAVPART bs=64 count=$NAVSIZE of=/disk/usb.*/$NAVVERSION.image
-# SSHSERVER=spam@xyz.com
-# rsync -r -v --progress --partial --append-verify $NAVMOUNT/. $SSHSERVER:~/$NAVVERSION/.
-######### END NAVIGON
-
-exit
+    NAVPART=$(cat /proc/mounts | grep opt/nav | head -n1 | cut -d " " -f1)
+    NAVMOUNT=$(cat /proc/mounts | grep opt/nav | head -n1 | awk '{print $2;}')
+    NAVSIZE=$(echo "$STATUS" | grep 'Online map package size:' | awk -F'Online map package size: ' '{print $2}' | awk '{print $1/64}')
+    NAVVERSION=$(cat $NAVMOUNT/VERSION | head -n1 | cut -d " " -f1)
+    echo "Version $NAVVERSION is mounted at $NAVPART $NAVMOUNT ($NAVSIZE)"
+     dd if=$NAVPART bs=64 count=$NAVSIZE of=/disk/usb.*/$NAVVERSION.image
+     SSHSERVER=spam@xyz.com
+     rsync -r -v --progress --partial --append-verify $NAVMOUNT/. $SSHSERVER:~/$NAVVERSION/.
+}
