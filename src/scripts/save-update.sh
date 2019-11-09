7a853f686082afd3ddba0b7d32e8dce33bc01332
Matt <Geczy@users.noreply.github.com>
2019-11-09 09:14:36 -0500

    Update save-update.sh

--- src/scripts/save-update.sh
+++ src/scripts/save-update.sh
@@ -7,6 +7,9 @@
 
 # Usage: bash save-update.sh HOST MODE &
 
+HOST="$1"
+MODE="$2"
+
 function die {
     echo "ERROR: $1" >&2
     exit 1
@@ -22,15 +25,11 @@ function initializeVariables {
     FTPSERVER="username:password@ftp.example.com:21"
 }
 
-
 function cleanFTPUsername { 
     # If FTP username has an @, replace with %40
     USERNAME=$(echo $FTPSERVER | cut -d: -f1)
     ENCODEDUSERNAME=${USERNAME/@/%40}
     FTPSERVER=${FTPSERVER/$USERNAME/$ENCODEDUSERNAME}
-
-    HOST="$1"
-    MODE="$2"
 }
 
 function validateIC {
@@ -40,24 +39,22 @@ function validateIC {
     fi
 }
 
-initializeVariables
-cleanFTPUsername
-validateIC
+function getFirmwareInfo {
+    PARTITIONPREFIX=$([ "$HOST" = ic ] && echo "mmcblk3p" || echo "mmcblk0p")
+    STATUS=$([ "$HOST" = ic ] && curl http://ic:21576/status || curl http://cid:20564/status)
+    NEWSIZE=$(echo "$STATUS" | grep 'Offline dot-model-s size:' | awk -F'size: ' '{print $2}' | awk '{print $1/64}')
+    NEWVER=$(echo "$STATUS" | awk -F'built for package version: ' '{print $2}' | sed 's/\s.*$//')
 
-PARTITIONPREFIX=$([ "$HOST" = ic ] && echo "mmcblk3p" || echo "mmcblk0p")
-STATUS=$([ "$HOST" = ic ] && curl http://ic:21576/status || curl http://cid:20564/status)
-NEWSIZE=$(echo "$STATUS" | grep 'Offline dot-model-s size:' | awk -F'size: ' '{print $2}' | awk '{print $1/64}')
-NEWVER=$(echo "$STATUS" | awk -F'built for package version: ' '{print $2}' | sed 's/\s.*$//')
-
-# Ty kalud for finding offline part number
-ONLINEPART=$(cat /proc/self/mounts | grep "/usr" | grep ^/dev/$PARTITIONPREFIX[12] | cut -b14)
-if [ "$ONLINEPART" == "1" ]; then
-    OFFLINEPART=2
-elif [ "$ONLINEPART" == "2" ]; then
-    OFFLINEPART=1
-else
-    die "Could not determine offline partition"
-fi
+    # Ty kalud for finding offline part number
+    ONLINEPART=$(cat /proc/self/mounts | grep "/usr" | grep ^/dev/$PARTITIONPREFIX[12] | cut -b14)
+    if [ "$ONLINEPART" == "1" ]; then
+        OFFLINEPART=2
+    elif [ "$ONLINEPART" == "2" ]; then
+        OFFLINEPART=1
+    else
+        die "Could not determine offline partition"
+    fi
+}
 
 function saveAPE {
     # Download gateway config in case it doesn't exist
@@ -85,25 +82,37 @@ function saveAPE {
     fi
 }
 
-if [ "$MODE" = internal ]; then
-    echo "Saving to /tmp/$NEWVER.image"
-    dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 of=/tmp/$NEWVER.image count=$NEWSIZE
-elif [ "$MODE" = usb ]; then
-    echo "Saving to /$NEWVER.image on usb"
-    sudo mount -o rw,noexec,nodev,noatime,utf8 /dev/sda1 /disk/usb.*/
-    dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 of=/disk/usb.*/$NEWVER.image count=$NEWSIZE
-    sync
-    umount /disk/usb.*/
-elif [ "$MODE" = ssh ]; then
-    echo "Saving to /tmp/$NEWVER.image on remote server via SSH"
-    dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | ssh $SSHSERVER "dd of=/tmp/$NEWVER.image"
-elif [ "$MODE" = ftp ]; then
-    saveAPE
-    echo "Saving to ~/$NEWVER.image on remote server via FTP"
-    dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | curl -T - ftp://$FTPSERVER/~/$NEWVER.image
-else
-    die "MODE must be one of usb | internal | ssh | ftp"
-fi
+function saveUpdate {
+    if [ "$MODE" = internal ]; then
+        echo "Saving to /tmp/$NEWVER.image"
+        dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 of=/tmp/$NEWVER.image count=$NEWSIZE
+    elif [ "$MODE" = usb ]; then
+        echo "Saving to /$NEWVER.image on usb"
+        sudo mount -o rw,noexec,nodev,noatime,utf8 /dev/sda1 /disk/usb.*/
+        dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 of=/disk/usb.*/$NEWVER.image count=$NEWSIZE
+        sync
+        umount /disk/usb.*/
+    elif [ "$MODE" = ssh ]; then
+        echo "Saving to /tmp/$NEWVER.image on remote server via SSH"
+        dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | ssh $SSHSERVER "dd of=/tmp/$NEWVER.image"
+    elif [ "$MODE" = ftp ]; then
+        saveAPE
+        echo "Saving to ~/$NEWVER.image on remote server via FTP"
+        dd if=/dev/$PARTITIONPREFIX$OFFLINEPART bs=64 count=$NEWSIZE | curl -T - ftp://$FTPSERVER/~/$NEWVER.image
+    else
+        die "MODE must be one of usb | internal | ssh | ftp"
+    fi
+}
+
+function main {
+    initializeVariables
+    cleanFTPUsername
+    validateIC
+    getFirmwareInfo
+    saveUpdate
+}
+
+main
 
 function saveNavigon {
     # @TODO: Add navigon image to arguments
