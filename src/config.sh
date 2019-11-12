b6e1e2e94dd12e3b05613732b0972babc31c1fe3
Matt <Geczy@users.noreply.github.com>
2019-11-12 07:33:21 -0500

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Changed from /home/ because a factory reset will wipe out Lunars
+# Changed from /home/ because a factory reset will wipe out Lunars in /home/ but will remain in /var/
 export homeOfLunars="/var/lunars"
