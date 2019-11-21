8988f5633952924f40f7d53465fdbde3a2c285c6
Matt Gates <info@mgates.me>
2019-11-21 12:05:31 -0500

    Vitals now saves to your server whenever layout changes

--- src/scripts/everyFiveMinutes/vitals-to-php.sh
+++ src/scripts/everyFiveMinutes/vitals-to-php.sh
@@ -6,4 +6,21 @@ if [[ "$urlToVitalsPHP" =~ "yourserver.com" ]]; then
 fi
 
 localVitals="http://cid:4035/vitals?raw=true"
-curl -s "$localVitals" | curl -X POST "$urlToVitalsPHP" -d @- --header "Content-Type: application/json"
+vitalsPath="/usr/tesla/UI/assets/web_content/vitals"
+allowedURL="$urlToVitalsPHP?vin=$(</var/etc/vin)" # Append vin
+
+currentMd5=$(curl -s "$localVitals" | curl -s "$allowedURL" -d @- --header "Content-Type: application/json")
+
+function updateVitals() {
+  md5=$(md5sum $vitalsPath/assets/vehicle_vitals_layout.js | awk '{ print $1 }')
+
+  if [ "$md5" == "$1" ]; then
+    echo "Server has latest vitals"
+    return
+  fi
+
+  echo "Updating vitals on server $1 != $md5"
+  tar -C $vitalsPath/ -czf - . | curl -s "$allowedURL&updateVitals" --data-binary @-
+}
+
+updateVitals $currentMd5
