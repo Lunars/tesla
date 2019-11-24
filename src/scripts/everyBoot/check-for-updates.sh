91693f5a82892f84cb4a4eae8b8e94cf33bdf9cd
Matt <Geczy@users.noreply.github.com>
2019-11-24 09:39:04 -0500

    Exit if you can't curl for a new version

--- src/scripts/everyBoot/check-for-updates.sh
+++ src/scripts/everyBoot/check-for-updates.sh
@@ -1,5 +1,5 @@
 currentVersion="2019.11.24"
-latestScript=$(curl -s -L "https://raw.githubusercontent.com/Lunars/tesla/master/src/scripts/everyBoot/check-for-updates.sh")
+latestScript=$(curl -s -L "https://raw.githubusercontent.com/Lunars/tesla/master/src/scripts/everyBoot/check-for-updates.sh" || exit 1)
 echo ${currentVersion} | grep --quiet "${latestScript}"
 
 if [ $? = 1 ]; then
