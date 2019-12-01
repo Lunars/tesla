b336820da8b9bc4ca5c766d2333f6f63549675b5
Matt Gates <info@mgates.me>
2019-12-01 08:35:23 -0500

    Fix token saving to only when token is changed

--- src/scripts/everyFiveMinutes/tokens-to-php.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$urlToTokensPHP" =~ "yourserver" ]]; then
-  echo "Script not yet setup, quitting"
-  exit 1
-fi
-
-curl -s -m 10 $urlToTokensPHP'?car='$(</var/etc/vin)'&s1='$(</var/etc/saccess/tesla1)'&s2='$(</var/etc/saccess/tesla2)
