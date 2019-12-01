93057998ad271a0735af6987e4a1d4cf4be5ccb1
Matt Gates <info@mgates.me>
2019-12-01 08:34:28 -0500

    shellcheck startstopvpn.sh

--- src/scripts/everyFiveMinutes/startstopvpn.sh
+++ src/scripts/everyFiveMinutes/startstopvpn.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 file="$homeOfLunars/tesla.ovpn"
-vpn=$(awk '/^dev /' $file | awk '{print $2}')
+vpn=$(awk '/^dev /' "$file" | awk '{print $2}')
 
 if [ -z "$vpn" ]; then
   echo "Script not yet setup, quitting"
   exit 1
 fi
 
-if grep -q "YOURIPADDRESSHERE" $file; then
+if grep -q "YOURIPADDRESSHERE" "$file"; then
   echo "Script not yet setup, quitting"
   exit 1
 fi
 
-iptables -D INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
-iptables -I INPUT -i $vpn -p tcp --dport 22 -j ACCEPT
-/sbin/start-stop-daemon --start --quiet --make-pidfile --oknodo --background --pidfile /var/run/lunars-vpn.pid --exec /usr/sbin/openvpn -- --config $file
+iptables -D INPUT -i "$vpn" -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -i "$vpn" -p tcp --dport 22 -j ACCEPT
+/sbin/start-stop-daemon --start --quiet --make-pidfile --oknodo --background --pidfile /var/run/lunars-vpn.pid --exec /usr/sbin/openvpn -- --config "$file"
