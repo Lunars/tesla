6bc7e84ee0e0d35b7762fe0e20b4764366645be0
Matt <Geczy@users.noreply.github.com>
2020-01-01 14:26:59 -0500

    Update autopilot-autosteer.sh
    Needs to run more often to work

--- src/scripts/everyBoot/autopilot-autosteer.sh
+++ src/scripts/everyBoot/autopilot-autosteer.sh
@@ -3,27 +3,14 @@
 # Allows autosteer at all speeds
 # Does not disable autopilot nag
 
-function watchRun() {
-  # Wait until QtCarServer is running
-  until pgrep QtCarServer; do
-    sleep 1
-  done
-
-  sdv GUI_disableAutosteerRestrictions true
-  echo "GUI_disableAutosteerRestrictions set to true"
-}
-
-function watchStop() {
-  # Wait until QtCarServer is NOT running
-  until [[ -z $(pgrep QtCarServer) ]]; do
-    sleep 1
-  done
-}
+KEY="GUI_disableAutosteerRestrictions"
+WANTSTATE="true"
 
 while true; do
-  # Run sdv if QtCarServer is running
-  watchRun
-
-  # Wait for it to restart
-  watchStop
+  STATE=$(lv $KEY | cut -d "\"" -f2)
+  if ! [ "$STATE" = "$WANTSTATE" ]; then
+    sdv $KEY $WANTSTATE
+    echo "Enabling $KEY $WANTSTATE"
+  fi
+  sleep 60
 done
