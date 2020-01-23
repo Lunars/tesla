4e409665e1928a3b124d717f069b07381d6ef942
E=MT2 <47479126+AsianTea@users.noreply.github.com>
2020-01-22 18:19:33 -0800

    Update listen-for-code.sh
    Added content for apn, needed for newer versions of firmware

--- src/scripts/everyBoot/listen-for-code.sh
+++ src/scripts/everyBoot/listen-for-code.sh
@@ -107,6 +107,7 @@ while true; do
       " offline")
         mv /home/tesla/.Tesla/car/cell_apn /home/tesla/.Tesla/car/cell_apn.bak
         touch /home/tesla/.Tesla/car/cell_apn
+        echo "junk" >> /home/tesla/.Tesla/car/cell_apn
         emit-reboot-gateway
         ;;
       " online")
