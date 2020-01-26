815d310924aaa3456e7445cc5adaee29b454d499
gryghst72 <54452464+gryghst72@users.noreply.github.com>
2020-01-26 14:04:27 -0500

    Updated off/online.sh
    Only allow offline to create one backup; only revert and reboot if offline.sh has been called.

--- src/scripts/misc/offline.sh
+++ src/scripts/misc/offline.sh
@@ -1,4 +1,8 @@
-mv /home/tesla/.Tesla/car/cell_apn /home/tesla/.Tesla/car/cell_apn.bak
-touch /home/tesla/.Tesla/car/cell_apn
-echo "junk" >> /home/tesla/.Tesla/car/cell_apn
-emit-reboot-gateway
+#!/bin/bash
+
+# Do not overwrite back up if already taken.
+if [ ! -f /home/tesla/.Tesla/car/cell_apn.bak ]; then
+	mv /home/tesla/.Tesla/car/cell_apn /home/tesla/.Tesla/car/cell_apn.bak
+	echo "junk" > /home/tesla/.Tesla/car/cell_apn
+	emit-reboot-gateway
+fi
--- src/scripts/misc/online.sh
+++ src/scripts/misc/online.sh
@@ -1,2 +1,7 @@
-mv /home/tesla/.Tesla/car/cell_apn.bak /home/tesla/.Tesla/car/cell_apn
-emit-reboot-gateway
+#!/bin/bash
+
+# revert and reboot only if initial backup was taken from offline.sh
+if [ -f /home/tesla/.Tesla/car/cell_apn.bak ]; then
+	mv /home/tesla/.Tesla/car/cell_apn.bak /home/tesla/.Tesla/car/cell_apn
+	emit-reboot-gateway
+fi
